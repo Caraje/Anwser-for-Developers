aab43ae9823232eb71ece76f688d8870fe9fa139
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-01-30 20:32:04 +0100

    fixeds

--- utils/spanishAnswer.js
+++ utils/spanishAnswer.js
@@ -1,14 +1,14 @@
 import { questionIa } from '../services/questionsIA'
-import { langTraductor } from '../services/traductor'
+import { LangTraductor } from '../services/traductor'
 
 export const spanishAnswer = async (query) => {
-  const queryTranslate = await langTraductor(query, 'en')
+  const queryTranslate = await LangTraductor(query, 'en')
   const queryEN = queryTranslate[0].translations[0].text
   const originalAnswer = await questionIa(queryEN)
   const msgError =
     'Se ha producido un error, le recomendamos repetir la pregunta en ingles.\nDisculpe las molestias'
 
-  const translate = await langTraductor(
+  const translate = await LangTraductor(
     originalAnswer
       .replaceAll('"', '|')
       .replaceAll('\n', '$')
