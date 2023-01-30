a9c39d760f8ef5b749fea9e20b08a0c9f24fc9dc
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-01-30 20:35:44 +0100

    refactor getAnswer

--- utils/getAnswer.js
+++ utils/getAnswer.js
@@ -1,30 +1,8 @@
+import { langDetect } from '../services/languageDetect'
 import { questionIa } from '../services/questionsIA'
 import { spanishAnswer } from './spanishAnswer'
 
 // ---------------
-const COHERE_API_KEY = import.meta.env.VITE_COHERE_API_KEY
-const COHERE_API_DETECT_LANGUAGE_URL = import.meta.env
-  .VITE_COHERE_API_DETECT_LANGUAGE_URL
-
-export const langDetect = (input) => {
-  const options = {
-    method: 'POST',
-    headers: {
-      accept: 'application/json',
-      'content-type': 'application/json',
-      authorization: `BEARER ${COHERE_API_KEY}`,
-    },
-    body: JSON.stringify({ texts: [input] }),
-  }
-
-  const results = fetch(COHERE_API_DETECT_LANGUAGE_URL, options)
-    .then((response) => response.json())
-    .then((response) => {
-      return response
-    })
-    .catch((err) => console.error(err))
-  return results
-}
 
 // ---------------
 
