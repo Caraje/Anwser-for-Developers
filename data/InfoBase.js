a6d74a26c7332eebbb0a2ac03aad7763f34f7568
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-01 14:19:14 +0100

    add dictionary infoBase

---
+++ data/InfoBase.js
@@ -0,0 +1,34 @@
+export const infoBase = {
+  infoHome: `Seguro que como desarrollador has tenido cientos de preguntas que has resuelto gracias a las busquedas en Google o Stackoverflow.
+
+  Aqui encontraras las respuestas a tus preguntas sobre desarrollo web de manera sencilla y directa, tan solo pregunta como lo harias a tu mentor o profesor y nos encargamos de resolvertela en cuestion de segundos.
+  
+  Resuelve tus dudas sobre Javascript, Css o cualquier otro lenguaje de programación.  
+  `,
+  moreInfo: {
+    title: 'Información',
+    textDesc: [
+      'Este proyecto ha sido creado con motivo del Hackathon de Enero 2023 creado por Midudev.',
+
+      'Se ha creado con la tecnologia de Inteligencia Artificial de Co:here.',
+
+      'La idea propuesta consiste en que el usuario puede hacer las preguntas sobre desarrollo web que tenga a la aplicación y mediante el uso de la inteligencia artificial de Co:here, se genera una respuesta acorde a lo aprendido por la IA.',
+
+      'Las respuestas generadas seran mas claras cuanto mas descriptiva sea la pregunta realizada.',
+
+      '    La IA creada por Co:here esta pensada actualmente para ser usada en Ingles, y es en este idioma como se obtienen las respuestas mas fieles. ',
+
+      'Sin embargo esta aplicacion esta pensada para ofrecer respuestas tanto a personas que saben ingles como a las que no, por lo que hace uso de una API de traduccion automatica de Microsoft, que traduce la pregunta del español al ingles al igual que la respuesta del ingles al español.',
+
+      'Desgraciadamente esta traduccion no siempre es la mas exacta y puede traer algun error.',
+
+      'En ocasiones las traducciones pueden generar un error, en ese caso recomendamos repetir la pregunta a la IA, pero lo ideal es que se repita en ingles, evitando asi posibles errores.',
+
+      'Esta aplicacion ha sido creada por Caraje y si te gusta puedes ver mas trabajos en la web www.carlosajenjo.es',
+
+      'Toda esta web esta libre de derechos de autor (expectuando los que puedan tener las tecnologias de Co:here y Microsoft, por lo demas, se libre para compiar lo que necesites, si es haciendo mencion, mucho mejor.',
+
+      '¡¡¡Ten un buen dia y gracias por visitar esta web !!!',
+    ],
+  },
+}
