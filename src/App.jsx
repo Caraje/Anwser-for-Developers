01b7a96b46bc115203a8510514d4833bb9937497
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-01 14:20:54 +0100

    refactorize Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { useRef } from 'react'
 import { useState } from 'react'
-import LoadingBar from '../components/LoadingBar'
+import FormComponent from '../components/FormComponent'
+import ModalInfo from '../components/ModalInfo'
+import TitleLogo from '../components/TitleLogo'
+import { infoBase } from '../data/InfoBase'
 import { getAnswer } from '../utils/getAnswer'
 
 import style from './styles/Home.module.css'
 
 function App() {
-  const [formValue, setFormValue] = useState('')
-  const [isDisabled, setIsDisabled] = useState(true)
+  const [formValue, setFormValue] = useState(infoBase.infoHome)
+  const [isDisabled, setIsDisabled] = useState(false)
+  const dialog = useRef(null)
   const msgError = `Tú consulta esta vacía o es demasiado corta.\nPor favor, introduce una consulta más larga`
 
   const handleForm = async (event) => {
@@ -25,90 +30,26 @@ function App() {
   return (
     <>
       <section className={style.container}>
-        <h1 className={style.title}>
-          Answer
-          <br />
-          <sup className={style.spanA}>for</sup>
-          <span className={style.spanB}>Developers</span>
-        </h1>
-
-        <form disabled className={style.questionForm} onSubmit={handleForm}>
-          <label clasName={style.inputQuestion}>
-            <input
-              className={style.inputForm}
-              name='question'
-              placeholder='Aqui va la pregunta'
-            />
-            <button
-              disabled={isDisabled}
-              className={isDisabled ? style.disabled : style.btn}
-            >
-              Enviar
-            </button>
-          </label>
-        </form>
-        {isDisabled && <LoadingBar />}
+        <TitleLogo />
+        <FormComponent isDisabled={isDisabled} handleForm={handleForm} />
+        <textarea
+          className={style.answersBox}
+          value={formValue}
+          onChange={handleForm}
+          readOnly
+          disabled
+        />
       </section>
-      {/* <main className={style.container}>
-        <section className={style.titles}>
-          <h1>Question.dev</h1>
-          <p>
-            Realiza todas las preguntas que tengas sobre el mundo del desarrollo
-            FrontEnd
-          </p>
-        </section>
-        <section className={style.questionContainer}>
-          <textarea
-            value={formValue}
-            onChange={handleForm}
-            readOnly
-            disabled
-            placeholder='Aqui se muestra la respuesta a la pregunta'
-          />
-          <form disabled className={style.questionForm} onSubmit={handleForm}>
-            <label>
-              Haz tu pregunta:
-              <input name='question' placeholder='Aqui va la pregunta' />
-            </label>
-            <button
-              disabled={isDisabled}
-              className={isDisabled ? style.disabled : style.btn}
-            >
-              {isDisabled ? 'Esperando respuesta' : 'Envia tu pregunta'}
-            </button>
-          </form>
-        </section>
-        <LoadingBar />
-      </main>
-      <footer className={style.footerWeb}>
-        <section>
-          App de{' '}
-          <a
-            href='https://www.carlosajenjo.es'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            www.carlosajenjo.es
-          </a>{' '}
-          para la Hackathon creada por{' '}
-          <a
-            href='https://twitter.com/midudev'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            midudev
-          </a>
-          <br />
-          Usando la tecnologia de{' '}
-          <a
-            href='https://cohere.ai/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Co:here
-          </a>
-        </section>
-      </footer> */}
+      <ModalInfo infoBase={infoBase} dialog={dialog} />
+
+      <button
+        className={style.btnInfo}
+        onClick={() => {
+          dialog.current.showModal()
+        }}
+      >
+        !
+      </button>
     </>
   )
 }
