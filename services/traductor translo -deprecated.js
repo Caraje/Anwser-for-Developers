b570204b7d2b648e9095c40efaa84ea9f18ca521
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-07 11:09:34 +0100

    replace Translate API

--- services/traductor translo -deprecated.js
+++ services/traductor translo -deprecated.js
@@ -2,21 +2,25 @@ export const langTraductor = (text, from, target) => {
   const TRANSLATE_API_KEY = import.meta.env.VITE_TRANSLATE_API_KEY
   const TRANSLATE_API_URL = import.meta.env.VITE_TRANSLATE_API_URL
   const encodedParams = new URLSearchParams()
-  encodedParams.append('source_language', from)
-  encodedParams.append('target_language', target)
+  encodedParams.append('from', from)
+  encodedParams.append('to', target)
   encodedParams.append('text', text)
   const options = {
     method: 'POST',
     headers: {
       'content-type': 'application/x-www-form-urlencoded',
       'X-RapidAPI-Key': TRANSLATE_API_KEY,
-      'X-RapidAPI-Host': TRANSLATE_API_URL,
+      'X-RapidAPI-Host': 'translo.p.rapidapi.com',
     },
     body: encodedParams,
   }
 
-  const traduction = fetch(`https://${TRANSLATE_API_URL}/translate`, options)
+  const traduction = fetch(
+    `https://${TRANSLATE_API_URL}/api/v3/translate`,
+    options
+  )
     .then((response) => response.json())
-    .catch((err) => console.error(err))
+    .catch((err) => err)
+
   return traduction
 }
--- utils/spanishAnswer.js
+++ utils/spanishAnswer.js
@@ -3,14 +3,17 @@ import { langTraductor } from '../services/traductor'
 
 export const spanishAnswer = async (query) => {
   const queryTranslate = await langTraductor(query, 'es', 'en')
-  if (!queryTranslate.ok)
+
+  if (queryTranslate.status !== 'success')
     return `Existe un error con la API encargada de las traducciones.
-Es posible que este operativo en breve, sin embargo le recomendamos repetir la pregunta en inglés.
-Disculpe las moletias`
-  const queryEN = queryTranslate.translated_text
+  Es posible que este operativo en breve, sin embargo le recomendamos repetir la pregunta en inglés.
+  Disculpe las moletias`
+
+  const queryEN = queryTranslate.data.translatedText
   const originalAnswer = await questionIa(queryEN)
   const msgError =
     'Se ha producido un error, le recomendamos repetir la pregunta en inglés.\nDisculpe las molestias'
+
   const translate = await langTraductor(
     originalAnswer
       .replaceAll('"', '|')
@@ -20,8 +23,8 @@ Disculpe las moletias`
     'es'
   )
 
-  if (!queryTranslate.translated_text) return msgError
-  const spanishAnswer = translate.translated_text
+  if (!queryTranslate.data.translatedText) return msgError
+  const spanishAnswer = translate.data.translatedText
   return spanishAnswer
     .replaceAll('|', '"')
     .replaceAll('$', '\n')
